cab8cbbaff7c2b223b3c69c869fd33bd52c455ea
Chearn <colbybhearn@gmail.com>
2013-06-03 21:45:46 -0500

    It was totally missing

---
+++ Validator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CbhLib
+{
+    public class Validator
+    {
+        public static bool isYear(string y)
+        {
+            try
+            {
+                int res = -1;
+                if (!int.TryParse(y, out res))
+                    return false;
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
